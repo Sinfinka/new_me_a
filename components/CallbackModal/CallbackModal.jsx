a3fc5f73907f0a33a317eabfb29e442dba09e72f
Sinfinka <inadaradar@gmail.com>
2024-07-05 11:41:02 +0300

    add phone countru with react phone input

--- components/CallbackModal/CallbackModal.jsx
+++ components/CallbackModal/CallbackModal.jsx
@@ -1,10 +1,34 @@
-import React, { useEffect } from "react";
+import React, { useRef, useEffect } from "react";
 import css from "./CallbackModal.module.css";
 import { Formik, Form, Field, ErrorMessage } from "formik";
-import * as Yup from "yup";
+import PhoneInput from "react-phone-number-input";
 import Button from "../Button/Button";
+import { validationSchema } from "./validationSchema";
 
 const CallbackModal = ({ isOpen, onClose, onSubmit }) => {
+  const modalRef = useRef(null);
+  const backdropRef = useRef(null);
+
+  const handleBackdropClick = (e) => {
+    if (e.target === backdropRef.current && isOpen) {
+      onClose();
+    }
+  };
+
+  useEffect(() => {
+    if (isOpen && backdropRef.current) {
+      backdropRef.current.addEventListener("click", handleBackdropClick);
+    } else if (backdropRef.current) {
+      backdropRef.current.removeEventListener("click", handleBackdropClick);
+    }
+
+    return () => {
+      if (backdropRef.current) {
+        backdropRef.current.removeEventListener("click", handleBackdropClick);
+      }
+    };
+  }, [isOpen, onClose]);
+
   if (!isOpen) {
     return null;
   }
@@ -14,18 +38,17 @@ const CallbackModal = ({ isOpen, onClose, onSubmit }) => {
     phone: "",
   };
 
-  const validationSchema = Yup.object({
-    name: Yup.string().required("Обязательно для заполнения"),
-    phone: Yup.string().required("Обязательно для заполнения"),
-  });
-
   const handleSubmit = (values, { setSubmitting }) => {
+    console.log(values);
+    const phoneNumber = values.phone;
+    console.log(phoneNumber);
+
     onSubmit(values, { setSubmitting });
   };
 
   return (
-    <div className={css.modalBackdrop}>
-      <div className={css.modal}>
+    <div ref={backdropRef} className={css.modalBackdrop}>
+      <div ref={modalRef} className={css.modal}>
         <div className={css.modalHeader}>
           <h5 className={css.modalTitle}>Обратный звонок</h5>
           <button type="button" className={css.closeButton} onClick={onClose}>
@@ -38,10 +61,12 @@ const CallbackModal = ({ isOpen, onClose, onSubmit }) => {
             validationSchema={validationSchema}
             onSubmit={handleSubmit}
           >
-            {({ isSubmitting }) => (
+            {({ isSubmitting, values }) => (
               <Form>
                 <div className={css.formGroup}>
-                  <label htmlFor="name">Имя</label>
+                  <label className={css.label} htmlFor="name">
+                    Имя
+                  </label>
                   <Field
                     type="text"
                     className={css.inputField}
@@ -56,14 +81,25 @@ const CallbackModal = ({ isOpen, onClose, onSubmit }) => {
                   />
                 </div>
                 <div className={css.formGroup}>
-                  <label htmlFor="phone">Телефон</label>
-                  <Field
-                    type="tel"
-                    className={css.inputField}
-                    id="phone"
-                    name="phone"
-                    placeholder="Ваш телефон"
-                  />
+                  <label className={css.label} htmlFor="phone">
+                    Телефон
+                  </label>
+                  <Field name="phone">
+                    {({ field, form }) => (
+                      <PhoneInput
+                        {...field}
+                        id="phone"
+                        name="phone"
+                        placeholder="Ваш телефон"
+                        className={css.customPhone}
+                        onChange={(value) => form.setFieldValue("phone", value)}
+                      />
+                    )}
+                  </Field>
+                  <div className={css.phoneNumberDisplay}>
+                    <p>Проверте Ваш номер телефона: {values.phone}</p>
+                  </div>
+
                   <ErrorMessage
                     name="phone"
                     component="div"
