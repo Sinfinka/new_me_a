936f6f09f04e67aed05042a59eebc6ef177eecb4
Sinfinka <inadaradar@gmail.com>
2024-07-19 17:55:43 +0300

    add bariatric services

---
+++ pages/services/bariatric/gastric-bypass/index.js
@@ -0,0 +1,20 @@
+import Head from "next/head";
+
+export default function GastricBupassPage() {
+  return (
+    <div>
+      <Head>
+        <title>Шунтирование желудка</title>
+        <meta name="description" content="Шунтирование желудка" />
+        {/* ЗМІНИТИ Open Graph и Twitter */}
+      </Head>
+      <div>
+        <h1>Шунтирование желудка</h1>
+        <p>
+          Мы специализируемся на бариатрической хирургии, которая помогает
+          пациентам достигать здоровья через потерю веса.
+        </p>
+      </div>
+    </div>
+  );
+}
---
+++ pages/services/bariatric/sleeve-gastrectomy/index.js
@@ -0,0 +1,20 @@
+import Head from "next/head";
+
+export default function SleeveGastrectomyPage() {
+  return (
+    <div>
+      <Head>
+        <title>Резекция желудка</title>
+        <meta name="description" content="Резекция желудка" />
+        {/* ЗМІНИТИ Open Graph и Twitter */}
+      </Head>
+      <div>
+        <h1>Резекция желудка</h1>
+        <p>
+          Мы специализируемся на бариатрической хирургии, которая помогает
+          пациентам достигать здоровья через потерю веса.
+        </p>
+      </div>
+    </div>
+  );
+}
